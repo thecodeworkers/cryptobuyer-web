b3a7565a3fc0fc7b021d2ad0b1e5e45b79cb6087
gibercode <giberlara@gmail.com>
2021-03-24 15:10:37 -0400

    main banner progress

---
+++ src/components/FirstBanner/index.tsx
@@ -0,0 +1,31 @@
+import styles from './styles.module.scss';
+
+const FirstBanner = ({data}) => {
+
+  return (
+    <>
+    <div className='_main'>
+      <div className={styles._content}>
+        <div className={styles._textParent}>
+          <h1 className={styles._title}> { data.title } </h1>
+          <p className={styles._subtitle} > { data.subtitle } </p>
+
+          <button className={styles._button}> {data?.button?.title} </button>
+        </div>
+      </div>
+    </div>
+
+    <style jsx>{`
+      ._main {
+        background-image: url(${data?.background?.mediaItemUrl});
+        background-repeat: no-repeat;
+        background-size: 100% 100%;
+        width:100%;
+        height: 100vh;
+      }
+    `}</style>
+    </>
+  )
+}
+
+export default FirstBanner;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,19 @@
 import { useSelector } from 'react-redux'
 import { wrapper } from '../store'
-import { Navbar } from '../components'
+import { Navbar, FirstBanner } from '../components'
 import { getResources } from '../store/actions'
 import Head from 'next/head'
 
 const Home = () => {
 
   const { page: { homePage: { home } } } = useSelector(state => state)
-
-  console.log(home)
-
   return (
     <div>
       <Head>
         <title>CryptoBuyer</title>
       </Head>
       <Navbar />
+      <FirstBanner data={home?.mainBanner}/>
       <div style={{backgroundColor: '#EFEFEF', width: '100%', height: '100vh', margin: 0, padding: 0}}>
 
       </div>
