30249358f819f7329f04460106b73296bd0612dd
MauriceMiot <maurice.yggdrasil@gmail.com>
2021-03-25 14:57:43 -0400

    fixed indexed

--- src/components/CommercesSlider/index.tsx
+++ src/components/CommercesSlider/index.tsx
@@ -1,8 +1,8 @@
 import styles from './styles.module.scss';
-import React from "react";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 const CommercesSlider = ({ section }) => {
 
