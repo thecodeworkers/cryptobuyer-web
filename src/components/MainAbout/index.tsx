8716dd3e9c7956bef4789ddd5aaa45e0ec4c663a
free1f <liseth.yggdrasil@gmail.com>
2021-03-25 13:03:25 -0400

    some styles

--- src/components/MainAbout/index.tsx
+++ src/components/MainAbout/index.tsx
@@ -1,21 +1,36 @@
 import styles from './styles.module.scss'
 
 const MainAbout = ({ data }) => {
-    function createMarkup() {
-        return {__html: data?.mainBanner.content};
+    const createMarkup = (text) => {
+        return {__html: text};
     }
+
+    const scrollToNextSection = () => {
+		var i = 10;
+		var int = setInterval(function () {
+			window.scrollTo(0, i);
+			i += 10;
+			if (i >= document.documentElement.clientHeight) clearInterval(int);
+		}, 20);
+	}
+
     console.log(data)
+
+    
     return (
     <div  style={{height: '100vh'}}>
         <div className={styles._bannerContainer} style={{ backgroundImage:`url(${data?.mainBanner?.background?.mediaItemUrl})` }}>
             <div className={styles._mainText}>
                 <h1 className={styles._mainTitle}>{data?.mainBanner?.title}</h1>
-                <div dangerouslySetInnerHTML={createMarkup()}></div>
+                <div dangerouslySetInnerHTML={createMarkup(data?.mainBanner.content)}></div>
             </div>
         </div>
         <div className={styles._subtitleContainer}>
-            <h2 className={styles._subtitleText}>{data?.subtitle}</h2>
-            <p>Conoce a nuestro <strong>equipo</strong></p>
+            <h2 className={styles._subtitleText} dangerouslySetInnerHTML={createMarkup(data?.subtitle)}></h2>
+            <div>
+                <p>Conoce a nuestro <strong>equipo</strong></p>
+                <img src={'images/icons/down-arrow.svg'} className={styles._downarrow} onClick={scrollToNextSection}></img>
+            </div>
         </div>
     </div>
     )
