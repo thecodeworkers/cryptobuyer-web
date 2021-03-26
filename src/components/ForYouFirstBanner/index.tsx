e9e0df1f04c9ef3210718726605db0300b415581
gibercode <giberlara@gmail.com>
2021-03-26 15:36:21 -0400

    banner with local image

---
+++ src/components/ForYouFirstBanner/index.tsx
@@ -0,0 +1,22 @@
+
+const ForYouFirstBanner = () => {
+
+  return (
+    <>
+    <div className='_main'></div>
+
+    <style jsx>{`
+      ._main {
+          background-image: url('/images/banners/banner-foryou.png');
+          background-repeat: no-repeat;
+          background-size: 100% 100%;
+          width:100%;
+          height: 100vh;
+        }
+      `}</style>
+    </>
+  )
+
+}
+
+export default ForYouFirstBanner;
---
+++ src/pages/for-you.tsx
@@ -0,0 +1,10 @@
+import { Navbar, ForYouFirstBanner } from '../components'
+
+const ForYou = () => (
+  <>
+    <Navbar color='#FFFFFF'/>
+    <ForYouFirstBanner />
+  </>
+)
+
+export default ForYou;
