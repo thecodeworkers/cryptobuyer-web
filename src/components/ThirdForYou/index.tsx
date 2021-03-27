635025ff7425e712effdcd6d59b2afe834f7c639
MauriceMiot <maurice.yggdrasil@gmail.com>
2021-03-27 15:08:11 -0400

    atm add

--- src/components/ThirdForYou/index.tsx
+++ src/components/ThirdForYou/index.tsx
@@ -8,24 +8,43 @@ const ThirdForYou = ({ section, content }) => {
 
 
   return (
-    <>
-      <div className={styles._container} ref={publicityBannerRef}>
-        <div className={styles._content}>
-          <div className={styles._leftContainer}>
-          <div className={styles._pointContainer}>
+      <div className={styles.main}>
+
+
+      <div className={styles._firstContainer} ref={publicityBannerRef}>
+        <div className={styles._firstContent}>
+          <div className={styles._firstLeftContainer}>
+          <div className={styles._cardsContainer}>
               <img src='images/resource/cards.svg'></img>
             </div>
           </div>
-          <div className={styles._rightContainer}>
-            <p className={styles._title}>{content?.title}</p>
-            <p className={styles._subtitle}>{content?.subtitle}</p>
-            <div className={styles._buttonContainer}>
-              <GeneralButton height={3} backgroundColor='#fff' textColor='#262833' text='Quiero enviar dinero' />
+          <div className={styles._firstRightContainer}>
+            <p className={styles._firstTitle}>Una tarjeta,varias monedas</p>
+            <p className={styles._firstSubtitle}>Recarga con tus criptomonedas y úsala en cualquier parte del mundo</p>
+            <div className={styles._firstButtonContainer}>
+              <GeneralButton height={3} backgroundColor='#2CACB3' textColor='#fff' text='Solicitar mi Visa' />
             </div>
           </div>
         </div>
       </div>
-    </>
+
+      <div className={styles._secondContainer} ref={publicityBannerRef}>
+        <div className={styles._secondContent}>
+          <div className={styles._secondLeftContainer}>
+          <p className={styles._secondTitle}>Monedas digitales al alcance de tus manos</p>
+            <p className={styles._secondSubtitle}>Compra Bitcoin, Dash y Litecoin en efectivo y en segundos gracias a los primeros cajeros automáticos de criptomonedas del mundo.</p>
+            <div className={styles._secondButtonContainer}>
+              <GeneralButton height={3} backgroundColor='#fff' textColor='#262833' text='¿Dónde están ubicados?' />
+            </div>
+          </div>
+          <div className={styles._secondRightContainer}>
+            <div className={styles._atmContainer}>
+              <img src='images/resource/atm.svg'></img>
+            </div>
+          </div>
+        </div>
+      </div>
+      </div>
   )
 }
 
