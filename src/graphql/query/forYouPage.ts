7ea33d1c86fb268c1a4a3d44342949b6cf022b82
Jorge Bastidas <jorgebastidas9@gmail.com>
2021-03-29 16:13:43 -0400

    separate pages and added second queries

---
+++ src/graphql/query/forYouPage.ts
@@ -0,0 +1,61 @@
+import { forYouId } from '@utils/pageIds'
+
+const forYou = `
+forYouPage:page(id: ${forYouId}) {
+  id
+  title
+  for_you {
+    mainBanner {
+      title
+      subtitle
+      background {
+        slug
+        mediaItemUrl
+      }
+      button {
+        title
+        link
+      }
+      lowTitle
+      lowSubtitle
+    }
+    secondBanner {
+      background {
+        slug
+        mediaItemUrl
+      }
+      title
+      subtitle
+      counter
+      button {
+        title
+        link
+      }
+      currencies {
+        image {
+          mediaItemUrl
+          slug
+        }
+      }
+    }
+    thirdBanner {
+      title
+      subtitle
+      button {
+        title
+        link
+      }
+    }
+    fourthBanner {
+      title
+      subtitle
+      button {
+        title
+        link
+      }
+    }
+  }
+}
+`
+
+export default forYou
--- src/pages/about-us.tsx
+++ src/pages/about-us.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { AboutUs } from '@components'
+import { wrapper } from '@store'
+import { getPages } from '@store/actions'
 
 const AboutUsPage = () => {
   const { page: { aboutPage: { about } } } = useSelector((state: any) => state)
   return <AboutUs content={about} />
 }
+
+export const getServerSideProps = wrapper.getServerSideProps(
+  ({ store }) => store.dispatch(getPages('aboutPage'))
+)
 
 export default AboutUsPage
