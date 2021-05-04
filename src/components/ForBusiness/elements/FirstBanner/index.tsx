879371909b41d732cff556de3ca2ab5a298e0d5e
MauriceMiot <maurice.yggdrasil@gmail.com>
2021-05-04 18:00:09 -0400

    CRYP-68

--- src/components/ForBusiness/elements/FirstBanner/index.tsx
+++ src/components/ForBusiness/elements/FirstBanner/index.tsx
@@ -30,11 +30,18 @@ const FirstBanner = ({ data, content, reference }) => {
             <p className={styles._customerSubtitles}>{content?.subtitle}</p>
           </div>
           <div className={styles._commercesContainer}>
-            <div className={styles._commercesLogo} style={{ backgroundImage: `url(${content?.commerces[0]?.image?.mediaItemUrl})` }}></div>
-            <div className={styles._commercesLogo} style={{ backgroundImage: `url(${content?.commerces[1]?.image?.mediaItemUrl})` }}></div>
-            <div className={styles._commercesLogo} style={{ backgroundImage: `url(${content?.commerces[2]?.image?.mediaItemUrl})` }}></div>
-            <div className={styles._commercesLogo} style={{ backgroundImage: `url(${content?.commerces[3]?.image?.mediaItemUrl})` }}></div>
-            <div className={styles._commercesLogo} style={{ backgroundImage: `url(${content?.commerces[4]?.image?.mediaItemUrl})` }}></div>
+            <div className={styles._firstRow}>
+              <div className={styles._commercesLogo} style={{ backgroundImage: `url(${content?.commerces[0]?.image?.mediaItemUrl})` }}></div>
+              <div className={styles._commercesLogo} style={{ backgroundImage: `url(${content?.commerces[1]?.image?.mediaItemUrl})` }}></div>
+              <div className={styles._commercesLogo} style={{ backgroundImage: `url(${content?.commerces[2]?.image?.mediaItemUrl})` }}></div>
+            </div>
+
+            <div className={styles._secondRow}>
+              <div className={styles._commercesLogo} style={{ backgroundImage: `url(${content?.commerces[3]?.image?.mediaItemUrl})` }}></div>
+              <div className={styles._commercesLogo} style={{ backgroundImage: `url(${content?.commerces[4]?.image?.mediaItemUrl})` }}></div>
+            </div>
+
+
           </div>
         </div>
       </div>
