86be6116085d4a3ca466fbfc11094ee81d60fd73
MauriceMiot <maurice.yggdrasil@gmail.com>
2021-05-08 00:39:17 -0400

    fixed

--- src/components/ForYou/elements/SecondBanner/index.tsx
+++ src/components/ForYou/elements/SecondBanner/index.tsx
@@ -42,7 +42,7 @@ const SecondBanner = ({ data, reference }) => {
         }
         @media(max-width: 575px) {
           ._main{
-            height: 50vh;
+            height: 60vh;
             background-image: url(${data?.backgroundResponsive?.mediaItemUrl});
 
           }
